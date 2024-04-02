b9a43f0e5874713c6c6bdab2c5221755d0fa14db
beauxq <beauxq@yahoo.com>
2024-04-01 18:54:57 -0700

    init

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use std::io::{BufRead, Write};
+
+const PORT: &str = "127.0.0.1:9998";
+
+
+fn do_server() -> Result<(), std::io::Error> {
+    let listener = std::net::TcpListener::bind(PORT)?;
+    println!("listening on http://{}", PORT);
+
+    for mut stream in listener.incoming().flatten() {
+        let addr = stream.peer_addr()?;
+        let mut reader = std::io::BufReader::new(&mut stream);
+        let mut line = String::new();
+        reader.read_line(&mut line)?;
+        match line.trim().split(' ').collect::<Vec<_>>().as_slice() {
+            ["GET", resource, "HTTP/1.1"] => {
+                println!("request from {} for {}", addr, resource);
+                loop {
+                    let mut line = String::new();
+                    reader.read_line(&mut line)?;
+                    if line.trim().is_empty() {
+                        break;
+                    }
+                }
+                let mut path = std::path::PathBuf::new();
+                path.push("static");
+                path.push(resource.trim_start_matches('/'));
+                if resource.ends_with('/') {
+                    path.push("index.html");
+                }
+                stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n")?;
+                stream.write_all(&std::fs::read(path)?)?;  // TODO: not secure
+            }
+            _ => todo!()
+        }
+    }
+
+    return Ok(());
+}
+
+
+fn main() {
+    if let Err(error) = do_server() {
+        println!("Error: {}", error);
+    }
+}
